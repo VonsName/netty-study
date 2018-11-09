a6062db84ff292bc90e1ab1621892448546ff12f
fjl <fengjianglin6817@dingtalk.com>
2018-11-09 09:46:21 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import io.netty.channel.socket.ServerSocketChannel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+}
---
+++ src/main/java/netty/TimeServer.java
@@ -0,0 +1,11 @@
+package netty;
+
+/**
+ * @author ： fjl
+ * @date ： 2018/11/9/009 9:45
+ */
+public class TimeServer {
+    public static void main(String[] args) {
+        
+    }
+}
